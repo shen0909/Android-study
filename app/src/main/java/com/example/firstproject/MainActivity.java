a2e49fc44f8126c07fd2ba22381813e9c841ea16
shen <1571817467@qq.com>
2023-11-03 17:55:35 +0800

    initial commit

---
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.tv_one);
+        textView.setText("小杨最厉害");
+
+    }
+}
