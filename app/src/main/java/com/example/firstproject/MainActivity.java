d7494171d8ea21ba832e5da15c3b5c924e241938
shen <1571817467@qq.com>
2023-11-06 17:59:03 +0800

    控件

--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.firstproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,9 +13,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.tv_one);
-        textView.setText("小杨最厉害");
+        setContentView(R.layout.edit_text);
+        /*TextView textView = findViewById(R.id.tv_one);
+        textView.setText("小杨最厉害");*/
 
+       /* Button button = findViewById(R.id.btn_one);
+        // 点击事件
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("你点击了"+button.getText());
+            }
+        });
+        // 长按事件
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                return false;
+            }
+        });
+        // 触摸方法
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return false;
+            }
+        });*/
     }
 }
